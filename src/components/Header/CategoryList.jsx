4d0c9177f6a18aa6db9081d03d398edb8efbb3f4
sonpham0812 <sonviruss2001@gmail.com>
2025-11-24 23:20:14 +0700

    fix bug get category list

--- src/components/Header/CategoryList.jsx
+++ src/components/Header/CategoryList.jsx
@@ -8,24 +8,27 @@ import { useNavigate } from "react-router-dom";
 const CategoryList = () => {
   const dispatch = useDispatch();
   const categories = useSelector(getCategoryList);
-  const categoryItems = categories?.map(item => ({ label: item.name, key: item.slug }))
-  const navigate = useNavigate()
+  const categoryItems = categories?.map((item) => ({
+    label: item.name,
+    key: item.slug,
+  }));
+  const navigate = useNavigate();
 
   const onClick = ({ key }) => {
-    navigate(`/category/${key}`)
-  }
+    navigate(`/category/${key}`);
+  };
 
   useEffect(() => {
     dispatch(fetchCategoryList());
   }, [dispatch]);
 
   return (
-    <Dropdown menu={{ categoryItems, onClick }}>
-      <a href="/" onClick={e => e.preventDefault()}>
-        <MenuOutlined style={{ fontSize: '24px' }} />
+    <Dropdown menu={{ items: categoryItems, onClick }} trigger={["click"]}>
+      <a href="/" onClick={(e) => e.preventDefault()}>
+        <MenuOutlined style={{ fontSize: "24px" }} />
       </a>
     </Dropdown>
-  )
+  );
 };
 
 export default CategoryList;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,54 +1,59 @@
-import { Badge, Button, Flex, Input } from "antd"
-import CategoryList from "./CategoryList"
-import HeaderTop from "./HeaderTop"
+import { Badge, Button, Flex, Input } from "antd";
+import CategoryList from "./CategoryList";
+import HeaderTop from "./HeaderTop";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategoryList, getCategoryList } from "../../store/categorySlice";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ShoppingCartOutlined } from "@ant-design/icons";
-import './index.scss';
+import "./index.scss";
 
 const Header = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const categories = useSelector(getCategoryList);
-    useEffect(() => {
-        dispatch(fetchCategoryList());
-    }, [dispatch]);
-    const onSearch = () => { }
-    const onClick = (category) => {
-        navigate(`/category/${category}`)
-    }
-    return (
-        <div className="header py-m bg-main">
-            <HeaderTop />
-            <Flex justify="space-between" align="center">
-                <Flex gap={2}>
-                    <CategoryList />
-                    <div>TempLogo</div>
-                </Flex>
-                <div className="search-categories">
-                    <Input.Search
-                        placeholder="Search your preferred items here"
-                        allowClear
-                        enterButton="Search"
-                        size="large"
-                        onSearch={onSearch}
-                    />
-                    <Flex justify="space-between">
-                        {categories?.slice(0, 10)?.map(item => (
-                            <Button variant="link" onClick={() => onClick(item.slug)}>
-                                {item.name}
-                            </Button>
-                        ))}
-                    </Flex>
-                </div>
-                <Badge count={0} showZero>
-                    <ShoppingCartOutlined style={{ fontSize: '24px', color: '#fff' }} />
-                </Badge>
-            </Flex>
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const categories = useSelector(getCategoryList);
+  useEffect(() => {
+    dispatch(fetchCategoryList());
+  }, [dispatch]);
+  const onSearch = () => {};
+  const onClick = (category) => {
+    navigate(`/category/${category}`);
+  };
+  return (
+    <div className="header py-m bg-main">
+      <HeaderTop />
+      <Flex justify="space-between" align="center">
+        <Flex gap={2}>
+          <CategoryList />
+          <div>TempLogo</div>
+        </Flex>
+        <div className="search-categories">
+          <Input.Search
+            placeholder="Search your preferred items here"
+            allowClear
+            enterButton="Search"
+            size="large"
+            onSearch={onSearch}
+          />
+          <Flex justify="space-between">
+            {categories?.slice(0, 10)?.map((item) => (
+              <Button
+                key={item.name}
+                color="default"
+                variant="link"
+                onClick={() => onClick(item.slug)}
+              >
+                {item.name}
+              </Button>
+            ))}
+          </Flex>
         </div>
-    )
-}
+        <Badge count={0} showZero>
+          <ShoppingCartOutlined style={{ fontSize: "24px", color: "#fff" }} />
+        </Badge>
+      </Flex>
+    </div>
+  );
+};
 
 export default Header;
